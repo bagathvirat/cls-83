5c48a6cd088c9477484f5d2f74a309afcce5da08
bagathvirat <81810396+bagathvirat@users.noreply.github.com>
2021-06-23 16:59:40 +0530

    Add files via upload

---
+++ screens/MyDonationScreen.js
@@ -0,0 +1,31 @@
+import React ,{Component} from 'react'
+import {View, Text,TouchableOpacity,ScrollView,FlatList,StyleSheet} from 'react-native';
+import {Card,Icon,ListItem} from 'react-native-elements'
+import MyHeader from '../components/MyHeader.js'
+import firebase from 'firebase';
+import db from '../config.js'
+
+export default class MyDonationScreen extends Component {
+   constructor(){
+     super()
+     this.state = {
+       donorId : firebase.auth().currentUser.email,
+       donorName : "",
+       allDonations : []
+     }
+     this.requestRef= null
+   }
+
+   static navigationOptions = { header: null };
+
+   getDonorDetails=(donorId)=>{
+     db.collection("users").where("email_id","==", donorId).get()
+     .then((snapshot)=>{
+       snapshot.forEach((doc) => {
+         this.setState({
+           "donorName" : doc.data().first_name + " " + doc.data().last_name
+         })
+       });
+     }) 
+    }
+}
